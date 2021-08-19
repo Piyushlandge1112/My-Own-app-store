156105e4f37d4594ace170e6035485837dd10f1c
Piyushlandge1112 <80309355+Piyushlandge1112@users.noreply.github.com>
2021-08-19 12:52:09 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,288 @@
+onEvent("button1", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("button2", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("image1", "click", function( ) {
+  open("https://www.facebook.com/");
+});
+onEvent("image2", "click", function( ) {
+  open("https://www.snapchat.com/");
+});
+onEvent("image3", "click", function( ) {
+  open("https://byjus.com/?utm_source=Google&utm_medium=CPC&utm_campaign=K12-Byjus-Brand-Desktop-India&utm_term=byjus&gclid=Cj0KCQjwsLWDBhCmARIsAPSL3_0X2Fc8rs0NLjgP0qEMDbhTJ_fMeIiI_HqmuqSmSVNnHSlYnDGywzEaAl2vEALw_wcB");
+});
+onEvent("image4", "click", function( ) {
+  open("https://www.instagram.com/");
+});
+onEvent("image5", "click", function( ) {
+  open("https://twitter.com/?lang=en");
+});
+onEvent("image6", "click", function( ) {
+  open("https://gaana.com/");
+});
+onEvent("image7", "click", function( ) {
+  open("https://www.hotstar.com/in");
+});
+onEvent("image8", "click", function( ) {
+  open("https://www.wikipedia.org/");
+});
+onEvent("image9", "click", function( ) {
+  open("https://www.apple.com/in/?afid=p238%7CsdUuvv563-dc_mtid_187079nc38483_pcrid_507827787007_pgrid_109516736379_&cid=aos-IN-kwgo-brand--slid---product-");
+});
+onEvent("image10", "click", function( ) {
+  open("https://scratch.mit.edu/");
+});
+onEvent("image11", "click", function( ) {
+  open("https://www.google.com/");
+});
+onEvent("button3", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("image12", "click", function( ) {
+  open("https://www.amazon.com/");
+});
+onEvent("image13", "click", function( ) {
+  open("https://www.phonepe.com/");
+});
+onEvent("image14", "click", function( ) {
+  open("https://www.youtube.com/");
+});
+onEvent("button4", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("button5", "click", function( ) {
+  setScreen("screen4");
+  
+});
+onEvent("button8", "click", function( ) {
+  setScreen("screen5");
+  
+});
+onEvent("button6", "click", function( ) {
+  setScreen("screen4");
+  
+});
+onEvent("button7", "click", function( ) {
+  setScreen("screen6");
+  
+});
+onEvent("button9", "click", function( ) {
+  setScreen("screen5");
+  
+});
+onEvent("button10", "click", function( ) {
+  setScreen("screen1");
+  
+});
+
+onEvent("button11", "click", function( ) {
+  setScreen("screen7");
+  
+});
+
+onEvent("button12", "click", function( ) {
+  setScreen("screen8");
+  
+});
+
+onEvent("button13", "click", function( ) {
+  setScreen("screen6");
+  
+});
+
+onEvent("button14", "click", function( ) {
+  setScreen("screen7");
+  
+});
+
+onEvent("button15", "click", function( ) {
+  setScreen("screen9");
+  
+});
+
+onEvent("button16", "click", function( ) {
+  setScreen("screen8");
+  
+});
+
+onEvent("button17", "click", function( ) {
+  setScreen("screen10");
+  
+});
+
+onEvent("button18", "click", function( ) {
+  setScreen("screen9");
+  
+});
+
+onEvent("image20", "click", function( ) {
+  open("https://watch.tatasky.com/");
+});
+
+onEvent("image21", "click", function( ) {
+  open("https://www.prabhatkids.edu.in/");
+});
+
+onEvent("image22", "click", function( ) {
+  open("https://brainly.in/");
+});
+onEvent("image23", "click", function( ) {
+  open("https://www.remove.bg/");
+});
+
+onEvent("image24", "click", function( ) {
+  open("https://spark.adobe.com/");
+});
+
+onEvent("image25", "click", function( ) {
+  open("https://meet.google.com/");
+});
+
+onEvent("image26", "click", function( ) {
+  open("https://translate.google.co.in/");
+});
+
+onEvent("image27", "click", function( ) {
+  open("https://classroom.google.com/");
+});
+
+onEvent("image28", "click", function( ) {
+  open("https://drive.google.com/");
+});
+
+onEvent("image29", "click", function( ) {
+  open("https://docs.google.com/");
+});
+
+onEvent("image30", "click", function( ) {
+  open("https://duo.google.com/");
+});
+
+onEvent("image31", "click", function( ) {
+  open("https://docs.google.com/");
+});
+
+onEvent("image32", "click", function( ) {
+  open("https://docs.google.com/");
+});
+
+onEvent("image33", "click", function( ) {
+  open("https://photos.google.com/");
+});
+
+onEvent("image34", "click", function( ) {
+  open("https://calendar.google.com/");
+});
+
+onEvent("image35", "click", function( ) {
+  open("https://www.google.co.in/shopping");
+});
+
+onEvent("image36", "click", function( ) {
+  open("https://news.google.com/");
+});
+
+onEvent("image37", "click", function( ) {
+  open("https://play.google.com/");
+});
+
+onEvent("image38", "click", function( ) {
+  open("https://www.google.co.in/");
+});
+
+onEvent("image39", "click", function( ) {
+  open("https://earth.google.com/web");
+});
+
+onEvent("image40", "click", function( ) {
+  open("https://mail.google.com/");
+});
+
+onEvent("image41", "click", function( ) {
+  open("https://mail.google.com/");
+});
+
+onEvent("image42", "click", function( ) {
+  open("https://keep.google.com/");
+});
+
+onEvent("image43", "click", function( ) {
+  open("https://docs.google.com/");
+});
+
+onEvent("image44", "click", function( ) {
+  open("https://mahakosh.gov.in/m/");
+});
+
+
+onEvent("image45", "click", function( ) {
+  open("https://www.google.co.in/maps/");
+});
+
+
+onEvent("image46", "click", function( ) {
+  open("https://firebase.google.com/");
+});
+
+
+onEvent("image47", "click", function( ) {
+  open("https://github.com/");
+});
+
+
+onEvent("image48", "click", function( ) {
+  open("https://zoom.us/");
+});
+
+onEvent("image49", "click", function( ) {
+  open("https://piyushlandge750.whjr.site/");
+});
+
+onEvent("image15", "click", function( ) {
+  open("https://code.whitehatjr.com/s/login");
+});
+onEvent("image17", "click", function( ) {
+  open("https://pay.google.com/gp/w/u/0/home/signup");
+});
+onEvent("image18", "click", function( ) {
+  open("https://web.whatsapp.com/");
+});
+onEvent("image19", "click", function( ) {
+  open("https://www.flipkart.com/");
+});
+
+onEvent("button19", "click", function( ) {
+  setScreen("screen11");
+  
+});
+
+onEvent("button20", "click", function( ) {
+  setScreen("screen12");
+  
+});
+onEvent("button21", "click", function( ) {
+  setScreen("screen13");
+  
+});
+
+
+onEvent("button22", "click", function( ) {
+  setScreen("screen11");
+  
+});
+
+onEvent("button23", "click", function( ) {
+  setScreen("screen11");
+  
+});
+
+onEvent("button24", "click", function( ) {
+  setScreen("screen10");
+  
+});
+
+onEvent("image52", "click", function( ) {
+  open("https://youtu.be/FrtlQWWzQnE");
+});
